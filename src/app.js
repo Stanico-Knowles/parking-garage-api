6313a95d557a467f5fe0e5fcd359e0cf57ba4280
Stanico Knowles <65465649+Stanico-Knowles@users.noreply.github.com>
2022-04-13 10:51:17 -0400

    remove commented code

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const ErrorHandler = require("./frameworks/errorHandler/errorHandler")
 const APIErrorHandler = require('./frameworks/errorHandler/APIError/APIErrorHandler')
 const port = 3000
 const db = require('./models/index')
 
 app.use(express.json());
 app.use(cors());
-
-// app.use(ErrorHandler);
-
 
 const cars = require('./garage/garageController')
 app.use('/', cars)
